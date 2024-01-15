1a12faf125358b593da239dbac51360354171ec2
mindy <minzhou16@gmail.com>
2024-01-15 12:25:41 -0500

    Added initial static file.

--- web-server/app.js
+++ web-server/app.js
@@ -1,11 +1,18 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const PORT = 3000
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.send("Welcome to express 🙈.")
+    return res.render("./pages/")
 })
 
 app.listen(PORT, () => {
