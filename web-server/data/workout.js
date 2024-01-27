81b3f206da8ac72dcba699acb2ceed7f29147f2e
mindy <minzhou16@gmail.com>
2024-01-27 15:22:25 -0500

    removed finally close part that creates connection closed error.

--- web-server/data/workout.js
+++ web-server/data/workout.js
@@ -7,34 +7,20 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 dotenv.config({ path: path.join(__dirname, "../.env") })
 
-const uri = `mongodb://${process.env.user}:${process.env.password}@localhost:27017/?authSource=workouts`
+const uri = `mongodb://${process.env.user}:${process.env.password}@localhost:27017/?authSource=${process.env.db}`
 
 const client = new MongoClient(uri)
 
 async function connect() {
     try {
-        console.log(uri)
-        const db = client.db("workouts")
+        const db = client.db(process.env.db)
         const exercises = db.collection("exercises")
         const cursor = exercises.find().sort({ day_number: 1 })
         const all_exercises = await cursor.toArray()
-        // for await (const exercise of exerciseCursor) {
-        //     // console.log(exercise)
-        //     all_exercises.push({
-        //         day_of_week: exercise.day_of_week,
-        //         type: exercise.type,
-        //         exercises: exercise.exercises,
-        //         rep_range: exercise.rep_range,
-        //         set_range: exercise.set_range,
-        //     })
-        // }
-        // console.log(all_exercises)
         return all_exercises
-    } finally {
-        await client.close()
+    } catch (err) {
+        console.log(err)
     }
 }
-
-// connect().catch(console.dir)
 
 export default connect
